2a4b280885450e01103af28d95231f5f0f5ad46f
Firangiz <firanhizmamiedova@gmail.com>
2023-09-04 21:11:02 +0400

    commit 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,11 @@ const currentQuestion = document.querySelector('#current-question');
 const totalQuestion = document.querySelector('#total-question');
 
 
+
 function Quiz(questionArr) {
     this.questions = questionArr;
     this.index = 0;
+    this.quizFinish = false;
 }
 
 //todo getQuestions butun suallarin icerisinden index-e = olani getirir
@@ -19,6 +21,8 @@ Quiz.prototype.getQuestion = function () {
 Quiz.prototype.increaseIndex = function () {
     if (this.questions.length - 1 > this.index) {
         this.index += 1;
+    } else {
+        this.quizFinish = true;
     }
 }
 
@@ -31,24 +35,48 @@ const quiz = new Quiz(questionLists);
 
 //todo
 function createQuiz() {
-    const currentQuestion = quiz.getQuestion();
-    questionTitle.innerHTML = quiz.index + 1 + '. ' + currentQuestion.title;
+    const findQuestion = quiz.getQuestion();
+    questionTitle.innerHTML = quiz.index + 1 + '. ' + findQuestion.title;
 
     let optionHtml = '';
-    for (let option in currentQuestion.options) {
+    for (let option in findQuestion.options) {
        optionHtml += `
-       <div class="option">
-            <b>${option}.</b> ${currentQuestion.options[option]}
+       <div class="option disabled" onclick='handleAnswer(this)'>
+            <b>${option}</b>. ${findQuestion.options[option]}
         </div>
        ` ;
     }
     questionOptions.innerHTML = optionHtml;
+    currentQuestion.innerHTML = quiz.index + 1;
+    totalQuestion.innerHTML = quiz.questions.length;
 }
 
 nextQuestion.addEventListener('click', function () {
+    nextQuestion.classList.remove('open');
     quiz.increaseIndex();
-    createQuiz();
+    if (!quiz.quizFinish) {
+        createQuiz();
+    }
 });
+
+function handleAnswer (event) {
+    const findQuestion = quiz.getQuestion();
+    if (event.querySelector('b').innerText === findQuestion.correct) {
+        event.classList.add('success');
+    } else {
+        event.classList.add('danger');
+    };
+
+    document.querySelectorAll('.option').forEach(function(el) {
+        el.classList.add('.disabled');
+    });
+
+    if (!quiz.quizFinish) {
+        nextQuestion.classList.add('open');
+    }
+
+    
+}
 
 createQuiz();
 
